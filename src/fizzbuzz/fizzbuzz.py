e35ade37f3611a0f7417ce27e18eea7d4ccd855b
safecorners <safecorners.mobile@gmail.com>
2024-02-28 10:09:20 +0900

    [fizzbuzz]03 - fizzbuzz(3)

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,2 +1,9 @@
-def fizzbuzz(n: int) -> int:
-    return 1
+from typing import Union
+
+
+def fizzbuzz(n: int) -> Union[int, str]:
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    return "Fizz"
--- tests/fizzbuzz/test_fizzbuzz.py
+++ tests/fizzbuzz/test_fizzbuzz.py
@@ -1,5 +1,13 @@
 from fizzbuzz.fizzbuzz import fizzbuzz
 
 
-def test_fizzbuzz() -> None:
+def test_fizzbuzz_1() -> None:
     assert fizzbuzz(1) == 1
+
+
+def test_fizzbuzz_2() -> None:
+    assert fizzbuzz(2) == 2
+
+
+def test_fizzbuzz_3() -> None:
+    assert fizzbuzz(3) == "Fizz"
